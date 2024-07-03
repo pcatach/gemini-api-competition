afa02bd2d17295c54661ea6f670aebed777cd37c
Patrick Catach <patrick.catach@gmail.com>
2024-07-03 20:11:12 +0100

    Fix show_frame and show_feed

--- src/camera.py
+++ src/camera.py
@@ -1,9 +1,21 @@
+"""
+>> for frame in Camera():
+>>     key = show_frame(frame)
+>>     if key == ord('q'):
+>>         break
+
+>> camera = Camera()
+>> camera.show_feed()
+"""
+
+
 import time
 
 import cv2
 import numpy as np
-from PIL import Image
 
+# The device ID is an integer that identifies which
+# video capturing device to use.
 VIDEO_CAPTURING_DEVICE_ID = 0
 
 
@@ -11,15 +23,35 @@ class FrameNotFoundError(Exception):
     pass
 
 
+def show_frame(frame: np.ndarray) -> int:
+    """For debugging purposes.
+
+    Creates a new window, displays the frame until any key is pressed.
+    Returns the ord() of the key that was pressed (useful if you want to
+    break out of a loop).
+    """
+    cv2.namedWindow("frame")
+    cv2.imshow("frame", frame)
+
+    # wait for any key press and stop displaying the frame
+    key = cv2.waitKey(0)
+    cv2.destroyWindow("frame")
+    # wait 1ms and destroy the window
+    # program hangs indefinitely if we don't call waitKey here.
+    cv2.waitKey(1)
+    return key
+
+
 class Camera:
     def __init__(self):
         self.video_capture = cv2.VideoCapture(VIDEO_CAPTURING_DEVICE_ID)
 
-    def close(self):
-        self.video_capture.release()
-
-    def is_open(self):
-        return self.video_capture.isOpened()
+    def __iter__(self) -> "Iterable[np.ndarray]":
+        while True:
+            try:
+                yield self.read_frame()
+            except FrameNotFoundError:
+                raise StopIteration
 
     def read_frame(self) -> np.ndarray:
         returned, frame = self.video_capture.read()
@@ -29,19 +61,38 @@ class Camera:
             )
         return frame
 
-    def show_frame(self):
+    def close(self):
+        """Close capturing device.
+
+        If this is not called the device will remain busy - won't be
+        able to invoke cv2.VideoCapture() again.
+        NOTE: release() is automatically called by VideoCapture destructor
+        when the program exits.
+        """
+        self.video_capture.release()
+
+    def is_open(self):
+        return self.video_capture.isOpened()
+
+    def show_current_frame(self):
+        """For debugging purposes."""
         frame = self.read_frame()
-        image = Image.fromarray(frame, "RGB")
-        image.show()
+        show_frame(frame)
 
     def show_feed(self):
+        """For debugging purposes.
+
+        Displays captured frame every 100ms until key 'q' is pressed
+        """
         cv2.namedWindow("feed")
+        cv2.resizeWindow("feed", 400, 300)
 
         while True:
             frame = self.read_frame()
             cv2.imshow("feed", frame)
-            if cv2.waitKey(100):
-                break
 
+            key = cv2.waitKey(100)
+            if key == ord('q'):
+                break
         cv2.destroyWindow("feed")
-        self.close()
+        cv2.waitKey(1)
