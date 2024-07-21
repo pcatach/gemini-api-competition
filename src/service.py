204225e64c170f6bc51cd112cc66a1abc7b1d2d4
Patrick Catach <patrick.catach@gmail.com>
2024-07-21 16:01:00 +0100

    Add skeleton for CCTV logger service

--- src/service.py
+++ src/service.py
@@ -1,41 +1,33 @@
-"""
-This module schedules model calls.
+import logging
 
-Every 30 seconds it will take a picture and invoke the model.
-We could do something like:
-while True:
-    do_thing()
-    time.sleep(30)
-but this is blocking and we want to be able to handle network requests in the meantime.
-Thus using Twisted. In twisted, the "reactor" is the event loop. We attach "callbacks"
-to the event loop that get called on certain events (like a time interval) and then
-return control to the event loop.
+from src.camera import Camera
+from src.model import Model, ModelChoices
+from src.utils import convert_frame_to_blob
 
-NOTE: requires setting the GOOGLE_API_KEY envvar
-"""
-import os
+TIME_INTERVAL = 40
+LOG = logging.getLogger("cctv_logger")
 
-import google.generativeai as genai
-from twisted.internet import reactor, task
 
-from camera import Camera, show_frame
-from model import ModelAPI, ModelChoices
-from utils import convert_frame_to_blob
+class CCTVLoggerServiceAdaptor:
+    """The adaptor integrates the logic for:
+    1. Taking a picture with the camera
+    2. Sending it to the model
+    3. Persisting the response to the database
 
-TIME_INTERVAL = 5
+    It must implement a run() method that will be invoked
+    by the twisted service, for instance
+    internet.TimerService(step=30, callable = adaptor.run)
+    """
 
-model = ModelAPI(ModelChoices.PRO)
-camera = Camera()
+    def __init__(self):
+        self.model = Model(ModelChoices.PRO)
+        self.camera = Camera()
 
-def send_picture():
-    print("Sending picture...")
-    frame = camera.read_frame()
-    show_frame(frame)
-    blob = convert_frame_to_blob(frame)
-    print(model.describe_image_from_blob(blob))
-
-# this attaches our function to the event loop as a callback
-loop = task.LoopingCall(send_picture)
-loop.start(TIME_INTERVAL)
-
-reactor.run()
+    def run(self):
+        LOG.info("Sending picture...")
+        frame = self.camera.read_frame()
+        blob = convert_frame_to_blob(frame)
+        response = self.model.describe_image_from_blob(blob)
+        LOG.info("Response:")
+        LOG.info(response)
+        # ...
---
+++ twistd.py
@@ -0,0 +1,50 @@
+"""Twisted application definition module.
+
+The twisted application framework provides a simple interface
+(service.Application) to run your code taking care of initialisation
+tasks such as running a reactor.
+
+Applications are run using the `twistd` CLI that comes bundled with Twisted
+installations. So it should be enough to run it with:
+$ /path/to/twistd --python twistd.py --nodaemon
+
+Note: you'll have to specify the full path to twistd (even if you're using
+a virtual environment).
+
+The --nodaemon flag prevents daemonisation, but if you have the daemon running
+you can terminate it using `kill $(cat twistd.pid)`.
+
+The Application itself is just a container for Services that handle your specific
+application logic (web servers, SSH clients, etc.).
+"""
+
+import logging
+
+# if '.' is not added to PYTHONPATH,
+# twistd won't do it for you so you'll get import errors.
+import sys
+
+sys.path.append(".")
+
+from twisted.application import internet, service
+from twisted.python import log
+
+from src.service import CCTVLoggerServiceAdaptor, TIME_INTERVAL
+
+logging.basicConfig(level=logging.INFO)
+
+top_service = service.MultiService()
+
+# service to take logs
+cctv_logger_adaptor = CCTVLoggerServiceAdaptor()
+cctv_logger_service = internet.TimerService(
+    step=TIME_INTERVAL, callable=cctv_logger_adaptor.run
+)
+cctv_logger_service.setServiceParent(top_service)
+
+# service to listen for connections
+# tcp_service = internet.TCPServer(...)
+# tcp_service.setServiceParent(top_service)
+
+application = service.Application("cctv_logger")
+top_service.setServiceParent(application)
