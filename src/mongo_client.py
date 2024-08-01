437ae8afa4e6c33073201306637c6922a8ab1818
Lucas Silva Simões <simoes.lucas.silva@gmail.com>
2024-08-01 22:25:28 +0100

    Add get_scenes_in_timerange, get_first_scene to MongoClient

--- src/mongo_client.py
+++ src/mongo_client.py
@@ -2,6 +2,7 @@ from datetime import datetime, timezone
 
 from pydantic import BaseModel as PydanticModel
 from pymongo import MongoClient as PymongoClient
+from pymongo import ASCENDING, DESCENDING
 
 from src.schemas import Scene
 
@@ -40,7 +41,27 @@ class MongoClient:
         res = pymongo_collection.insert_one(document.dict())
         return res
 
-    def get_latest_scene(self, db=None, collection=None):
+    def _get_scene(self, db_collection=None, reverse=True, verbose=False):
+
+        order = -1 if reverse else 1
+        res = db_collection.find_one(sort={"timestamp": order}) or {}
+        if verbose:
+            return res.get("scene"), res.get("timestamp")
+        return res.get("scene")
+
+    def get_first_scene(self, db=None, collection=None, verbose=False):
+        """
+        Retrieve the first scene from the database
+
+        :param db: str: mongo database name (if not provided, uses object default)
+        :param collection: str: mongo collection name (if not provided, uses object default)
+        """
+        pymongo_collection = self.get_collection(db=db, collection=collection)
+        return self._get_scene(
+            db_collection=pymongo_collection, reverse=False, verbose=verbose
+        )
+
+    def get_latest_scene(self, db=None, collection=None, verbose=False):
         """
         Retrieve the latest scene from the database
 
@@ -48,9 +69,36 @@ class MongoClient:
         :param collection: str: mongo collection name (if not provided, uses object default)
         """
         pymongo_collection = self.get_collection(db=db, collection=collection)
+        return self._get_scene(db_collection=pymongo_collection, verbose=verbose)
 
-        res = pymongo_collection.find_one(sort={"timestamp": -1}) or {}
-        return res.get('scene')
+    def get_scenes_in_timerange(
+        self, start_time, end_time=None, db=None, collection=None, verbose=False
+    ):
+        """
+        Retrieve the scenes from the database with timestamp within a range.
+
+        :param start_time: datetime: left side of timestamp range
+        :param end_time: datetime: right side of timestamp range (if not provided, uses now)
+        :param db: str: mongo database name (if not provided, uses object default)
+        :param collection: str: mongo collection name (if not provided, uses object default)
+        """
+
+        pymongo_collection = self.get_collection(db=db, collection=collection)
+
+        end_time = end_time or datetime.now(tz=timezone.utc)
+        # Might need to convert times if timezone-naive to timezone-aware
+        if start_time.tzinfo is None:
+            start_time = datetime.fromtimestamp(start_time.timestamp(), timezone.utc)
+        if end_time.tzinfo is None:
+            end_time = datetime.fromtimestamp(end_time.timestamp(), timezone.utc)
+        assert start_time < end_time, "Not a valid timestamp range!"
+
+        res = pymongo_collection.find(
+            {"timestamp": {"$gte": start_time, "$lte": end_time}}
+        )
+        if verbose:
+            return [(doc["scene"], doc["timestamp"]) for doc in res]
+        return [doc["scene"] for doc in res]
 
     def get_collection(self, db=None, collection=None):
         db = db or self.default_db
