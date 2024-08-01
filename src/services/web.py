3a0621fb50e9505e9b4d99962b8e923014db3858
Lucas Silva Simões <simoes.lucas.silva@gmail.com>
2024-08-01 22:55:48 +0100

    Add _check_repeated to CCTVLoggerServer

--- src/services/web.py
+++ src/services/web.py
@@ -1,8 +1,10 @@
 import json
 
+from collections import Counter
 from twisted.web import resource
 
 from src.mongo_client import MongoClient
+from src.utils import today_start, sanitise_string
 
 
 class CCTVLoggerServer(resource.Resource):
@@ -14,3 +16,28 @@ class CCTVLoggerServer(resource.Resource):
         response = {"data": scene}
         request.setHeader("Content-Type", "application/json")
         return json.dumps(response).encode("utf-8")
+
+    def _check_repeated(self, start_time=None, end_time=None, thresh=5):
+        """
+        Returns a tuple with persons and vehicles that repeateldy shwo up in time range.
+
+        :param start_time: datetime: left side of timestamp range (default: start of today)
+        :param end_time: datetime: right side of timestamp range (default: now)
+        :param thresh: int: minimum number of counts to consider a repeat pattern 'common'
+            (default: 5)
+        """
+        start_time = today_start if start_time is None else start_time
+        scenes_in_range = self.client.get_scenes_in_timerange(start_time, end_time)
+        persons = Counter()
+        vehicles = Counter()
+        for scene in scenes_in_range:
+            persons += Counter(
+                sanitise_string(p.get("clothes")) for p in scene.get("persons")
+            )
+            vehicles += Counter(
+                (v.get("type"), v.get("color")) for v in scene.get("vehicles")
+            )
+
+        common_persons = {p: count for p, count in persons.items() if count > thresh}
+        common_vehicles = {p: count for p, count in persons.items() if count > thresh}
+        return common_persons, common_vehicles
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timezone
+
 import cv2
 import google.generativeai as genai
 
@@ -6,3 +8,12 @@ def convert_frame_to_blob(frame: "np.ndarray") -> genai.protos.Blob:
     """Converts an array representing a frame to a Blob object."""
     _, buffer = cv2.imencode(".png", frame)
     return genai.protos.Blob(mime_type="image/png", data=buffer.tobytes())
+
+
+def today_start():
+    today = datetime.now().date()
+    return datetime(today.year, today.month, today.day, tzinfo=timezone.utc)
+
+
+def sanitise_string(s):
+    return s.replace("t-", "").replace("and ", "")
